476d616379d47ca7235b3ace117a728b19631071
Khambay Phasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-04 22:04:06 +0000

    completed conditional logic

---
+++ conditional_logic.js
@@ -0,0 +1,21 @@
+// Working on conditional logic with if statements
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+        return "No, that was false";
+}
+
+// Testing trueOrFalse function
+trueOrFalse(true);
+trueOrFalse(false);
